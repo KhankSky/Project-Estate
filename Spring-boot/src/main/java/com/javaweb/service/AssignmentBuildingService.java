fcf5f5bc1a0762e6d16119b7094bd4bc1923073d
KhanhSky <pkhanhsnd037@gmail.com>
2024-06-25 09:09:58 +0700

    done

--- Spring-boot/src/main/java/com/javaweb/service/AssignmentBuildingService.java
+++
@@ -1,8 +0,0 @@
-package com.javaweb.service;
-
-import com.javaweb.model.dto.AssignmentBuildingDTO;
-
-public interface AssignmentBuildingService {
-    void updateAssignment (AssignmentBuildingDTO assignmentBuildingDTO);
-    void deleteByBuildingIds(Long[] ids);
-}
--- Spring-boot/src/main/java/com/javaweb/service/RentAreaService.java
+++
@@ -1,9 +0,0 @@
-package com.javaweb.service;
-
-import com.javaweb.entity.BuildingEntity;
-import com.javaweb.model.dto.BuildingDTO;
-
-public interface RentAreaService {
-    void create (BuildingDTO buildingDTO, BuildingEntity buildingEntity);
-    void deleteByBuilidngIds (Long[] ids);
-}
